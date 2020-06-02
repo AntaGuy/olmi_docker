8d8c01ab4b68788801b1d21739669b5c42481b8b
Guilherme <dasilva@antadis.com>
2020-06-02 09:45:49 +0200

    Sf extrabundle + guzzle

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,6 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle::class => ['all' => true],
 ];
